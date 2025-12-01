9e59dc30062ca0c70eded605d217a7ebaeec2ebd
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-12-01 23:34:19 +0530

    Dashboard Route created

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import {useEffect} from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
-import { motion } from 'framer-motion';
 import Home from './pages/Home';
 import Features from './Pages/Features';
-import Workflow from './Pages/Workflow'; // Adjusted based on previous component creation
+import Workflow from './Pages/Workflow'; 
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import AuthLayout from './Layout/AuthLayout';
 import MainLayout from './Layout/MainLayout';
+import ProtectedRoute from './Components/ProtectedRoute';
+import Dashboard from './Pages/Dashboard';
+ 
+
 import './App.css';
 import './index.css';
 
+
 const App = () => {
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      console.log("User is logged in ");
+    } else {
+      console.log("User is NOT logged in ");
+    }
+  });
+
   return (
     <div className="body-wrapper">
       <div className="ambient-glow" />
@@ -29,6 +44,11 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Route>
+
+        <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+
       </Routes>
     </div>
   );
