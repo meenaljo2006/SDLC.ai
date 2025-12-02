2d021b75233f4275c382188c0fd8b9073ac8108b
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-12-02 18:48:23 +0530

    Fixed API endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // '/api' se shuru hone wali har request ko target URL par bhejega
       '/api': {
         target: 'https://sdlc.testproject.live',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
+      '/auth': {
+        target: 'https://sdlc.testproject.live',
+        changeOrigin: true,
+        secure: false,
+      }
     },
   },
 })
