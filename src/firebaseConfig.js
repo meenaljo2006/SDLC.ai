27bf4e316b53a040b5d77d0e76478e99e04fb450
meenaljo2006 <meenaljoshi2006@gmail.com>
2025-12-29 05:44:02 +0530

    Fixing

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,14 +5,14 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCh0VvvykIo4_h_OgcbdEVSH6dmCW5mzPw",
-  authDomain: "sdlcai-3501a.firebaseapp.com",
-  projectId: "sdlcai-3501a",
-  storageBucket: "sdlcai-3501a.firebasestorage.app",
-  messagingSenderId: "867454965992",
-  appId: "1:867454965992:web:7e17dc4460092d9454115d"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCh0VvvykIo4_h_OgcbdEVSH6dmCW5mzPw",
+//   authDomain: "sdlcai-3501a.firebaseapp.com",
+//   projectId: "sdlcai-3501a",
+//   storageBucket: "sdlcai-3501a.firebasestorage.app",
+//   messagingSenderId: "867454965992",
+//   appId: "1:867454965992:web:7e17dc4460092d9454115d"
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig); 
